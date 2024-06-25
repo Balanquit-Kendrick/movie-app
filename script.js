5691f3e0be3dc267cecd90b9a9d8b43a03b87e1c
unknown <zxckendrick123@gmail.com>
2024-06-25 13:43:25 +0800

    changed js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const API_KEY = '0ac2dd6e01833f7f8b1c57b368edaf18'; // Replace with your TMDb API key
 const API_URL = 'https://api.themoviedb.org/3/movie/popular';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
-const limit = 24;
+const API_PAGE = 1;
+const counter = 0;
+const limit = 40;
 
-function searchMovies() {
-    fetch(`${API_URL}?api_key=${API_KEY}&language=en-US&page=1`)
-        .then(response => response.json())
-        .then(data => {
-            // Limit the results to the first 20 movies
-            const limitedMovies = data.results.slice(0, limit);
-            displayMovies(limitedMovies);
-        })
-        .catch(error => console.error('Error:', error));
+async function searchMovies() {
+
+    try {
+        const response1 = await fetch(`${API_URL}?api_key=${API_KEY}&language=en-US&page=${API_PAGE}`);
+        const data1 = await response1.json();
+        const movies = data1.results;
+        // console.log(`Number of movies on page 1: ${movies.length}`);
+        
+        if (movies.length<limit){
+        // Limit the results to the required number of movies
+        const limitedMovies = movies.slice(0, limit);
+        displayMovies(limitedMovies)};
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 function displayMovies(movies) {
