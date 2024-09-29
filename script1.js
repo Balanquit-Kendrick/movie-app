49361c73b03643c21ef4ac00f1bf9197e4956134
unknown <zxckendrick123@gmail.com>
2024-09-30 00:16:57 +0800

    imported script1.js

---
+++ script1.js
@@ -0,0 +1,48 @@
+const API_KEY = '0ac2dd6e01833f7f8b1c57b368edaf18'; 
+const API_URL = 'https://api.themoviedb.org/3/movie/popular';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+const limit = 40;
+
+function fetchMovies() {
+    const promises = [];
+
+    // Fetching movies from page 1 and page 2
+    for (let page = 1; page <= 2; page++) {
+        promises.push(fetch(`${API_URL}?api_key=${API_KEY}&language=en-US&page=${page}`)
+            .then(response => response.json()));
+    }
+
+    Promise.all(promises)
+        .then(results => {
+            // Combining movies from both pages
+            const allMovies = results.flatMap(result => result.results);
+            const limitedMovies = allMovies.slice(0, limit); // Limit to 40 movies
+            console.log('limitedMovies', limitedMovies);
+            
+            displayMovies(limitedMovies);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function displayMovies(movies) {
+    const moviesContainer = document.getElementById('moviesContainer');
+    moviesContainer.innerHTML = '';
+
+    if (movies.length === 0) {
+        moviesContainer.innerHTML = '<p>No movies found.</p>';
+        return;
+    }
+
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.className = 'slider-nav box bg-slate-300 align-text m-2 content-center text-center mx-5 w-[100px]';
+
+        const moviePoster = document.createElement('img');
+        moviePoster.src = movie.poster_path ? `${IMAGE_URL}${movie.poster_path}` : 'https://via.placeholder.com/100x150';
+        
+        movieElement.appendChild(moviePoster);
+        moviesContainer.className = 'grid grid-cols-8 ';
+        moviesContainer.appendChild(movieElement);
+    });
+}
+fetchMovies();
